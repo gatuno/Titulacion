0231b1bfc06c6dc1da0bbd93566dc658d09c61f4
Félix Arreola Rodríguez <fgatuno.123@gmail.com>
2013-07-15 13:31:04 +0000

    Agrego método para recuperar un Plan de estudios

--- src/Titulacion/PlanEstudio.php
+++ src/Titulacion/PlanEstudio.php
@@ -8,4 +8,20 @@ class Titulacion_PlanEstudio extends Gatuf_Model {
 		$this->_getConnection ();
 		$this->tabla = 'Planes_Estudio';
 	}
+	
+	public function getPlan ($id) {
+		$req = sprintf ('SELECT * FROM %s WHERE id=%s', $this->getSqlTable (), Gatuf_DB_IntegerToDb ($id, $this->_con));
+		
+		if (false === ($rs = $this->_con->select($req))) {
+			throw new Exception($this->_con->getError());
+		}
+		
+		if (count ($rs) == 0) {
+			return false;
+		}
+		foreach ($rs[0] as $col => $val) {
+			$this->$col = $val;
+		}
+		return true;
+	}
 }
